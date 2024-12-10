3f876a2a25387fcaee95934e3cfe5909fab9d20b
isaac boateng <isboateng@hotmail.com>
2024-12-10 11:29:29 +0000

    add profile model

--- src/Meme.Domain/Meme.Domain.Models/MemePageModel.cs
+++ src/Meme.Domain/Meme.Domain.Models/MemePageModel.cs
@@ -8,9 +8,6 @@
 
         public string? Name { get; set; }
 
-        /// <summary>
-        /// also called contract address
-        /// </summary>
         public string? ContractAddress { get; set; }
 
         public string? BuyUrl { get; set; }
@@ -18,6 +15,30 @@
         public string? PathUrl { get; set; }
 
         public AboutSectionModel? About { get; set; }
+
+        public HomeSectionModel? HomeSection { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public PageStatus Status { get; set; }
+
+        public Dictionary<string, string>? Metadata { get; set; }
+
+        public List<PageImageModel> Images { get; set; } = new List<PageImageModel>();
+    }
+
+
+    public class ProfileModel : IModelItem
+    {
+        public string? Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? ContractAddress { get; set; }
+
+        public string? BuyUrl { get; set; }
+
+        public string? PathUrl { get; set; }
 
         public HomeSectionModel? HomeSection { get; set; }
 
