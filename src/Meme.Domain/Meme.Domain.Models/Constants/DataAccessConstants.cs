17208f16b3bd5319ce88bdffb7b3a11c43ac8a4c
isaac boateng <isboateng@hotmail.com>
2024-12-12 11:24:20 +0000

    added constants

---
+++ src/Meme.Domain/Meme.Domain.Models/Constants/DataAccessConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace Meme.Domain.Models.Constants
+{
+    public class DataAccessConstants
+    {
+        public const string DatabaseName = "MemeHub";
+
+        public static string MemePagesCollection = "MemePages";
+
+        public static string ProfileCollection = "Profiles";
+
+        public static string UserCollection = "Users";
+    }
+}
