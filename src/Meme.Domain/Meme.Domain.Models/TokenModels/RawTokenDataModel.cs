7110fecea51e182c2057bc49179a2dea684593ce
isaac boateng <isboateng@hotmail.com>
2024-12-27 10:18:48 +0000

    added token data models

---
+++ src/Meme.Domain/Meme.Domain.Models/TokenModels/RawTokenDataModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Meme.Domain.Models.TokenModels
+{
+    public class RawTokenDataModel
+    {
+        public string? Signature { get; set; }
+        public string? Mint { get; set; }
+        public string? TraderPublicKey { get; set; }
+        public string? TxType { get; set; }
+        public double InitialBuy { get; set; }
+        public string? BondingCurveKey { get; set; }
+        public double VTokensInBondingCurve { get; set; }
+        public double VSolInBondingCurve { get; set; }
+        public double MarketCapSol { get; set; }
+        public string? Name { get; set; }
+        public string? Symbol { get; set; }
+        public string? Uri { get; set; }
+    }
+}
---
+++ src/Meme.Domain/Meme.Domain.Models/TokenModels/TokenDataModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meme.Domain.Models.TokenModels
+{
+    public class TokenDataModel
+    {
+        public string? Name { get; set; }
+        public string? Symbol { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+        public bool ShowName { get; set; }
+        public string? CreatedOn { get; set; }
+        public string? Website { get; set; }
+        public RawTokenDataModel? RawData { get; set; }
+    }
+}
