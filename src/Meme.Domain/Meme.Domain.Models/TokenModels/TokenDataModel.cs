d3e52344d06795ff4e92344014a1d39a29a320e0
isaac boateng <isboateng@hotmail.com>
2025-06-15 09:24:16 +0100

    guid back to string

--- src/Meme.Domain/Meme.Domain.Models/TokenModels/TokenDataModel.cs
+++ src/Meme.Domain/Meme.Domain.Models/TokenModels/TokenDataModel.cs
@@ -8,7 +8,7 @@ namespace Meme.Domain.Models.TokenModels
 {
     public class TokenDataModel
     {
-        public Guid Id { get; set; }
+        public string Id { get; set; }
         public string? Name { get; set; }
         public string? Symbol { get; set; }
         public string? Description { get; set; }
